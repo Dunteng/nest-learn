cb2e756782f9e2a9506056a008496de7ea279407
Dunteng <lindunteng@gmail.com>
2021-09-05 21:29:23 +0800

    fix: 配置swagger

--- project/src/app.controller.ts
+++ project/src/app.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
+@ApiTags('app总模块')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
+  @ApiTags('getHello')
+  @ApiOperation({
+    summary: '测试接口'
+  })
   getHello(): string {
     return this.appService.getHello();
   }
